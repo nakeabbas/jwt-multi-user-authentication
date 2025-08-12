b5cb33e7a570486ac4b86ac5c94419737b002f19
nakeabbas <96688121+nakeabbas@users.noreply.github.com>
2025-08-12 11:28:07 +0500

    add logs file

--- spring-local-mysql/src/main/java/com/example/app/controller/UserController.java
+++ spring-local-mysql/src/main/java/com/example/app/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.app.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,15 +9,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v0")
 public class UserController {
 
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/user")
     @PreAuthorize("hasRole('ROLE_USER')")
     public String userEndpoint() {
+        logger.info("Process Start User Role :" ,System.currentTimeMillis());
         return "Hello User!";
     }
 
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String adminEndpoint() {
+        logger.info("Process Start Admin Role :" ,System.currentTimeMillis());
         return "Hello Admin!";
     }
 }
